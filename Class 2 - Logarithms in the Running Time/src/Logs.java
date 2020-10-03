c8c97a5eb7748bc1307e46bea5c2258fc28e9e15
Mamadou <mamadou236@gmail.com>
2020-10-02 20:32:24 -0400

    Exemples of hash functions for strings

--- Class 2 - Logarithms in the Running Time/src/Logs.java
+++ Class 2 - Logarithms in the Running Time/src/Logs.java
@@ -35,8 +35,24 @@ public class Logs
         return m;
     }
 
+    // Efficient exponentiation
+    public static long pow(long x, int n)
+    {
+        if(n == 0)
+            return 1;
+        if(n == 1)
+            return x;
+        if(n % 2 == 0)
+            return pow(x * x, n / 2);
+        else
+            return pow(x * x, n / 2) * x;
+    }
+
+
     public static void main(String[] args) {
         Integer[] arr = new Integer[]{-100, 1, 2, 3, 4, 5, 6, 10, 46};
         System.out.println( "Binary Search: " + (binarySearch(arr, -100) == 0));
+        System.out.println( "GCD: " + (gcd(50, 15) == 5));
+        System.out.println( "Pow: " + (pow(3, 18) == 387420489));
     }
 }
